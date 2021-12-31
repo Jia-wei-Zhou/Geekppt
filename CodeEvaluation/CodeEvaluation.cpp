0c5b04f1eb7231bf8ead2d77a9d864e6ca5a9354
wzf6667 <1207343523@QQ.com>
2021-12-31 00:33:42 +0000

    read and writefile read and writecodeEva implementation tasks published

--- CodeEvaluation/CodeEvaluation.cpp
+++ CodeEvaluation/CodeEvaluation.cpp
@@ -25,3 +25,53 @@ std::string CodeEvaluation::executeAndGetFromCmd(std::string cmd) {
     }
     return result;
 }
+
+std::string CodeEvaluation::readTxt(std::string address) const {
+    std::ifstream ifile(address);
+    std::ostringstream buf;
+    char ch;
+    while (buf && ifile.get(ch))
+        buf.put(ch);
+    return buf.str();
+}
+
+bool CodeEvaluation::createAndWriteFile() {
+    std::string address = changeSuffix(language_);
+    if (address == "") {
+        return false;
+    }
+    std::ofstream ofs;
+    ofs.open(address, std::ios::out);
+    if (!ofs.is_open()) {
+        return false;
+    }
+    ofs << code_ << std::endl;
+    ofs.close();
+    return true;
+}
+std::string CodeEvaluation::changeSuffix(LanguageType language) {
+    std::string address = "";
+    if (language == CPP) {
+        address = address_.replace(address_.length() - 3, address_.length(), "cpp");
+    }
+    if (language == JAVA) {
+        address = address_.replace(address_.length() - 3, address_.length(), "java");
+    }
+    if (language == PYTHON) {
+        address = address_.replace(address_.length() - 3, address_.length(), "py");
+    }
+    return address;
+}
+int main()
+{
+    std::string address = "cfileff.txt";
+    std::string str;
+    enum LanguageType m = PYTHON;
+
+    CodeEvaluation c = CodeEvaluation(m, address);
+    str = c.readTxt(address);
+    c.createAndWriteFile();
+    std::cout << str << std::endl;
+
+
+}
