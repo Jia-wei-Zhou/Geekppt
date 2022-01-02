28a5767c5514b0cd49878e911333b663d1dd9206
Jiawei Zhou <lesliezhou0406@gmail.com>
2022-01-02 17:03:36 +0000

    Restore resource files
    accidentally removed resource files last commit

---
+++ CodeEvaluation/sampleCode1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "I'm sample code 1! Hello world!" << endl;
+    return 0;
+}
---
+++ CodeEvaluation/sampleText1.txt
@@ -0,0 +1,2 @@
+Hi, this is sample text 1!!!
+I want to be a .cpp file!
