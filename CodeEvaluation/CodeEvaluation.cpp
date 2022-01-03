474c6dfaeaf049cb62984b674704234a2c144899
luowx0905 <slywl3@163.com>
2022-01-03 01:32:28 +0000

    Add CMakeLists

--- CodeEvaluation/CodeEvaluation.cpp
+++ CodeEvaluation/CodeEvaluation.cpp
@@ -1,7 +1,4 @@
 #include "CodeEvaluation.h"
-#include <fstream>
-#include <stdexcept>
-#include <filesystem>
 
 /*  Comment: this function can be used in function generateCmakeFile,
     whereas it is recommended to use the same method in generateCmakeFile
@@ -43,14 +40,14 @@ std::string CodeEvaluation::generateCompileCommand(std::string const& compiler) 
     compileCmd += extractFilename(address_);
 
     switch (language_) {
-    case JAVA:
-        compileCmd += ".java";
-        return compileCmd;
-    case CPP:
-        compileCmd += ".cpp";
-        break;
-    default:
-        break;
+        case JAVA:
+            compileCmd += ".java";
+            return compileCmd;
+        case CPP:
+            compileCmd += ".cpp";
+            break;
+        default:
+            break;
     }
 
     compileCmd += " -o ";
@@ -89,11 +86,11 @@ std::string CodeEvaluation::generateRunCommand(std::string const& filename, std:
 
 // TODO
 void CodeEvaluation::generateCmakeFile(const std::string& project_name,
-    const std::string& main_file,
-    const std::string& output_cmake_path,
-    std::vector<std::string>& libs,
-    const int cpp_standard,
-    const std::string& cmake_mini_version) {
+                                       const std::string& main_file,
+                                       const std::string& output_cmake_path,
+                                       std::vector<std::string>& libs,
+                                       const int cpp_standard,
+                                       const std::string& cmake_mini_version) {
     // Determine if the validity of the output cmake lists
     if (!output_cmake_path.ends_with("CMakeLists.txt")) {
         throw std::runtime_error("Error in the path for output CMakeLists.txt");
@@ -146,11 +143,9 @@ void CodeEvaluation::executeInCmdLine(std::string const& cmd) {
 }
 
 
-
 std::string CodeEvaluation::executeAndGetFromCmd(std::string const& cmd) {
     // defend empty compile cmd (e.g. language_ is PYTHON)
     if (cmd.length() == 0) { return ""; }
-std::string CodeEvaluation::executeAndGetFromCmd(std::string cmd) {
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(&cmd[0], "r"), pclose);
@@ -163,6 +158,7 @@ std::string CodeEvaluation::executeAndGetFromCmd(std::string cmd) {
     return result;
 }
 
+
 std::string CodeEvaluation::runCode(std::string const& input) {
     try {
         filename_ = extractFilename(address_);
@@ -171,7 +167,7 @@ std::string CodeEvaluation::runCode(std::string const& input) {
         executeInCmdLine(compileCmd);
         std::string runCmd = generateRunCommand(filename_, input);
         return executeAndGetFromCmd(runCmd);
-    } 
+    }
     catch(std::runtime_error errors) {
         std::cerr << "Error message:" << errors.what() << '\n';
         throw errors;
@@ -188,7 +184,7 @@ std::string CodeEvaluation::runCode(std::string const& address, std::string cons
         executeInCmdLine(compileCmd);
         std::string runCmd = generateRunCommand(filename_, input);
         return executeAndGetFromCmd(runCmd);
-    } 
+    }
     catch(std::runtime_error errors) {
         std::cerr << "Error message:" << errors.what() << '\n';
         throw errors;
@@ -231,46 +227,4 @@ std::string CodeEvaluation::changeSuffix(LanguageType language) {
         address = address_.replace(address_.length() - 3, address_.length(), "py");
     }
     return address;
-=======
-void CodeEvaluation::generateCmakeFile(const std::string& project_name,
-                                       const std::string& main_file,
-                                       const std::string& output_cmake_path,
-                                       std::vector<std::string>& libs,
-                                       const int cpp_standard,
-                                       const std::string& cmake_mini_version) {
-    // Determine if the validity of the output cmake lists
-    if(!output_cmake_path.ends_with("CMakeLists.txt")) {
-        throw std::runtime_error("Error in the path for output CMakeLists.txt");
-    }
-
-    std::ofstream output(output_cmake_path);
-    if(output.fail()) {
-        throw std::runtime_error("Error in opening output files");
-    }
-
-    // Generate CMakeLists.txt
-    output << "cmake_minimum_required(VERSION " << cmake_mini_version << ")\n";
-    output << "project(" << project_name << ")\n\n";
-    output << "set(CMAKE_CXX_STANDARD " << cpp_standard << ")\n\n";
-
-    std::vector<std::string> lib_names;
-    for(auto&& lib : libs) {
-        int separator_index = lib.find_last_of(std::filesystem::path::preferred_separator);
-        separator_index = (separator_index == std::string::npos) ? 0 : separator_index + 1;
-
-        int dot_index = lib.find_last_of('.');
-        std::string lib_name = lib.substr(separator_index, dot_index - separator_index);
-        lib_names.push_back(lib_name);
-
-        output << "add_library(" << lib_name << " " << lib << ")\n";
-    }
-
-    output << "\nadd_executable(main " << main_file << ")\n";
-
-    output << "\ntarget_link_libraries(main ";
-    std::for_each(lib_names.begin(), lib_names.end(), [&](auto&& lib) { output << lib << " "; });
-    output << ")\n";
-
-    // Finish generating CMakeLists.txt
-    output.close();
 }
